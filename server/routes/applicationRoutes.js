c7c25db6af39d902eda2cd2ff43874578988cecb
DHARUNPV <dharunpv007@gmail.com>
2025-09-07 07:48:39 +0530

    applicationRoutes.js

--- server/routes/applicationRoutes.js
+++ server/routes/applicationRoutes.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const applicationController = require('../controllers/applicationController');
 const auth = require('../middleware/auth');
 
-// Protected route to get logged-in user applications
+// Apply for a job
+router.post('/', auth, applicationController.applyJob);
+
+// Get logged-in user applications
 router.get('/my-applications', auth, applicationController.getUserApplications);
 
 module.exports = router;
